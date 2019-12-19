4eaaeabcc6b523977ffd10e27c72a604c9ce8e32
דוד ברטלר <ddkill8@gmail.com>
2019-12-19 12:34:27 +0200

    Add project files.

---
+++ BE/GuestRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BE
+{
+    public class GuestRequest
+    {
+        int GuestRequestKey;
+        string PrivateName;
+        string MailAddress;
+        string Status;
+        DateTime RegistrationDate;
+        DateTime EntryDate;
+        DateTime ReleaseDate;
+        string  Area;
+        string SubArea;
+        string Type;
+        int Adults;
+        int Children;
+        string Pool;
+        string Jacuzzi;
+        string Garden;
+        string ChildrensAttractions;
+        public override string ToString() { return "the request details:\n" +
+                "Guest Request Key:"+ GuestRequestKey+"\n"+; }
+    }
+}
