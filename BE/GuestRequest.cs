f45bd74d7f8bbfc5b4f294b688c816cbfddcb8db
דוד ברטלר <ddkill8@gmail.com>
2019-12-19 13:15:43 +0200

    david changes

--- BE/GuestRequest.cs
+++ BE/GuestRequest.cs
@@ -4,23 +4,35 @@ namespace BE
 {
     public class GuestRequest
     {
-        int GuestRequestKey;
-        string PrivateName;
-        string MailAddress;
-        string Status;
-        DateTime RegistrationDate;
-        DateTime EntryDate;
-        DateTime ReleaseDate;
-        string  Area;
-        string SubArea;
-        string Type;
-        int Adults;
-        int Children;
-        string Pool;
-        string Jacuzzi;
-        string Garden;
-        string ChildrensAttractions;
-        public override string ToString() { return "the request details:\n" +
-                "Guest Request Key:"+ GuestRequestKey+"\n"+; }
+        int GuestRequestKey { get;  }
+        string PrivateName { get; set; }
+        string MailAddress { get; set; }
+        string Status { get; set; }
+        DateTime RegistrationDate { get; set; }
+        DateTime EntryDate { get; set; }
+        DateTime ReleaseDate { get; set; }
+        string  Area { get; set; }
+        string SubArea { get; set; }
+        string Type { get; set; }
+        int Adults { get; set; }
+        int Children { get; set; }
+        string Pool { get; set; }
+        string Jacuzzi { get; set; }
+        string Garden { get; set; }
+        string ChildrensAttractions { get; set; }
+        public override string ToString() 
+        {
+            return "the request details:\n" +
+                "Guest Request Key: "+ GuestRequestKey+"\n"+
+                " PrivateName: "+ PrivateName+ "\n" +
+                "MailAddress: " + MailAddress + "\n" + 
+                "Status: " + Status+ "\n" + " RegistrationDate: " + RegistrationDate +
+                " EntryDate: "+ EntryDate + " ReleaseDate: "+ ReleaseDate+ "\n" +
+                " Area: " + Area+  "SubArea: "+ SubArea+ "\n" +
+                " Type :" + Type+ " Adults :"+ Adults + 
+                " Children :"+ Children+ "\n" +
+                "Pool: " + Pool+ " Jacuzzi: "+ Jacuzzi+ 
+                " Garden: "+ Garden+ " ChildrensAttractions: "+ChildrensAttractions; 
+        }
     }
 }
---
+++ BE/HostingUnit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BE
+{
+    class HostingUnit
+    {
+      int HostingUnitKey { get; }
+       Host Owner { get; }
+        string HostingUnitName { get; }
+        bool [,] Dairy = new bool[12,31];     
+        public override string ToString() 
+        { 
+            return "HostingUnitKey: "+HostingUnitKey+
+                "/n Owner: "+Owner +"HostingUnitName: "+HostingUnitName +
+                for (int i = 0; i<12; i++)
+			   {
+              for (int j = 0; j<31; j++)
+
+                    Console.WriteLine(Dairy[i, j]);
+               }
+         }
+    }
+}
