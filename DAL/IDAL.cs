a87d537a124ded3f5594b98d15016a8bc426fc27
dovbaker <dovbaker@gmail.com>
2019-12-22 13:21:37 +0200

    dfgh

--- DAL/IDAL.cs
+++ DAL/IDAL.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace DAL
 {
-    class IDAL
+    interface Idal
     {
+        void NewCustomerRequest(BE.GuestRequest TheGuestRequest);
+        void UpdateCustomerRequest(BE.GuestRequest TheGuestRequest);
+        void AddNewHostingUnit(BE.HostingUnit TheHostingUnit);
+        void DeleteHostingUnit(BE.HostingUnit TheHostingUnit);
+        void UpdateHostingUnit(BE.HostingUnit TheHostingUnit);
+        void NewOrder(BE.Order TheOrder);
+        void UpdateDateOrder(BE.Order TheOrder);
+        List<BE.HostingUnit> ListOfAllHostingUnits();
+        List<BE.GuestRequest> ListOfAllGuestRequest();
+        List<BE.Order> ListOfAllOrder();
+        List<BE.BankBranch> ListOfAllBankBranch();
     }
 }
+
+
