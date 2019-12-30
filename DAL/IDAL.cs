f91cbb3da8f1d4725f718e1a103da5635f6ca367
dovbaker <dovbaker@gmail.com>
2019-12-30 10:56:17 +0200

    hh

--- DAL/IDAL.cs
+++ DAL/IDAL.cs
@@ -5,19 +5,21 @@ using System.Linq;
 
 namespace DAL
 {
-    interface IDAL
+    interface Idal
     {
         void NewGuestRequests(BE.GuestRequest TheGuestRequest);
         void UpdateGuestRequests(BE.GuestRequest TheGuestRequest);
         void AddNewHostingUnit(BE.HostingUnit TheHostingUnit);
-        void DeleteHostingUnit(BE.HostingUnit TheHostingUnit);
+        void DeleteHostingUnit(BE.HostingUnit TheHostingUnit);//למה יש רק פונ מחיקה אחת ??
         void UpdateHostingUnit(BE.HostingUnit TheHostingUnit);
         void NewOrder(BE.Order TheOrder);
         void UpdateDateOrder(BE.Order TheOrder);
-        List<BE.HostingUnit> ListOfAllHostingUnits();
-        List<BE.GuestRequest> ListOfAllGuestRequest();
-        List<BE.Order> ListOfAllOrder();
-        List<BE.BankBranch> ListOfAllBankBranch();
+        List<BE.HostingUnit> ListOfHostingUnits();
+        List<BE.GuestRequest> ListOfGuestRequest();
+        List<BE.Order> ListOfOrder();
+        List<BE.BankBranch> ListOfBankBranch();
+
+      
     }
 }
 
