5c8bd3651390f3a5f1e5e904f8ebda512b3636fd
דוד ברטלר <ddkill8@gmail.com>
2020-01-03 12:19:13 +0200

    dsf

--- BE/BankBranch.cs
+++ BE/BankBranch.cs
@@ -6,10 +6,10 @@ namespace BE
 {
     public class BankBranch
     {
-        string BankName { get; }
-        int BranchNumber { get; }
-        string BranchAddress { get; }
-        string BranchCity { get; } 
+       public string BankName { get; }
+        public int BranchNumber { get; }
+        public string BranchAddress { get; }
+        public string BranchCity { get; } 
       public BankBranch(string BankName2, int BranchNumber2, string BranchAddress2, string BranchCity2)
         {//temperery till we will get from real banks
             BankName = BankName2; BranchNumber = BranchNumber2;
