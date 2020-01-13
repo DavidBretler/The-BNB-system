92e59c514df84288997fc26fc3f913f6d1705416
דוד ברטלר <ddkill8@gmail.com>
2020-01-13 11:11:28 +0200

    סיום הזמנה חדשה

--- DAL/Cloning.cs
+++ DAL/Cloning.cs
@@ -4,33 +4,57 @@ using System.Text;
 using BE;
 namespace DAL
 {
-    public static class Cloning 
-    {//hosting unit order gestrequest
-        //public static BE.HostingUnit Clone(this BE.HostingUnit original)
-        //{
-        //    BE.HostingUnit target = new BE.HostingUnit();
-        //    target.setHostingUnitKey(original.getHostingUnitKey()) ;
-        //    target.setOwner(original.getOwner());
-        //    target.setHostingUnitName(original.getHostingUnitName());
-        //    target.setNumOfRooms(original.getNumOfRooms());
-        //    target.setNumOfBeds(  original.getNumOfBeds());         
-        //    target.setPool(original.getPool());
-        //    target.setJacuzzi(original.getJacuzzi());
-        //    target.setArea(original.getArea());
-        //    target.setGarden(original.getGarden());
-        //    target.setAirConditioner(original.getAirConditioner());
-        //    target.setChildrensAttractions (original.getChildrensAttractions());
-        //    target.setType (original.getType());
-        //    target.setHikes(original.getHikes());
-        //    target.setDiary(original.GetDiary());    
-        //    return target;
-        //}      
-       //     public static Clonable Clone(this Clonable original)
+    public static class Cloning
+    {
+        public static BE.BankAccount Clone(this BE.BankAccount original)
+        {
+            BE.BankAccount target = new BankAccount();
+
+
+            target.BankName = original.BankName;
+            target.BranchNumber = original.BranchNumber;
+            target.BranchAddress = original.BranchAddress;
+            target.BranchCity = original.BranchCity;
+            target.BankAccountNumber = original.BankAccountNumber;
+            return target;
+
+        }
+        public static BE.GuestRequest Clone(this BE.GuestRequest original)
+        {
+            BE.GuestRequest target = new BE.GuestRequest();
+            original.PrivateName = target.PrivateName;
+            original.FamilyName = target.FamilyName;
+            original.MailAddress = target.MailAddress;
+            original.Status = target.Status;
+            original.RegistrationDate = target.RegistrationDate;
+            original.EntryDate = target.EndDate;
+            original.EndDate = target.EndDate;
+            original.Type = target.Type;
+            original.Area = target.Area;
+            original.Adults = target.Adults;
+            original.Children = target.Children;
+            original.Pool = target.Pool;
+            original.Jacuzzi = target.Jacuzzi;
+            original.Garden = target.Garden;
+            original.ChildrensAttractions = target.ChildrensAttractions;
+            original.GuestRequestKey = target.GuestRequestKey;
+            return target;
+        }
+        //public static BE.Order Clone(this BE.Order original)
+        //   {
+        //        BE.Order target = new BE.Order(original.Status, original.CreateDate, original.OrderDate, original.HostingunitKey, original.GuestrequestKey, original.Orderkey);
+
+
+        //         return target;
+        //   }
+        //    public static BE.HostingUnit Clone(this BE.HostingUnit original)
         //    {
-              //  Clonable target = Activator.CreateInstance(original.GetType());
-                //...
-             //   return target;
-         //   }       
-        //and so on for each entity…
+        //        BE.HostingUnit target = new BE.HostingUnit(original.Owner, original.Area, original.HostingUnitName, original.Hostingunitkey);
+        //        target.Diary = original.Diary;
+
+        //        return target;
+        //    }
+        //};
     }
 }
+
