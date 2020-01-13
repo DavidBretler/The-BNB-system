fb30d09f8c9b24fa457d4fd28462528db2e84ffd
דוד ברטלר <ddkill8@gmail.com>
2020-01-13 12:36:32 +0200

    asd

--- DAL/IDAL.cs
+++ DAL/IDAL.cs
@@ -7,8 +7,7 @@ namespace DAL
 {
 
     public interface  IDAL
-    {
-        GuestRequests FinedGuestRequestByKEY(int key);
+    {       
         void DeleteHostingUnit(int hostUnitKey);
         void DeleteGuestRequests(BE.GuestRequest TheGuestRequest);
         void Deleteorder(BE.Order TheOrder);
